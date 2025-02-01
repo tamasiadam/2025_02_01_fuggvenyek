812e0d1e20462c927ced968fa5021209f467ced6
Tamási Ádám <TamasiAdam@pg.local>
2025-02-01 09:42:10 +0100

    kesz orai

--- 02_feladat.py
+++ 02_feladat.py
@@ -1,11 +1,20 @@
 #2. Feladat
 #Írj egy programot, amely tartalmaz egy 'paros_e' nevű függvényt, amely True értékkel tér vissza, ha a paraméterként átvett listaelemek (egész számok) között van páros, ellenkező esetben a visszatérési érték False! A program tartalmazza a függvény hívását is!
 
-def paros_e(liszt):
-    if any(szam % 2 == 0 for szam in liszt):
-        return True
-    else:
-        return False
+# def paros_e(liszt):
+#     if any(szam % 2 == 0 for szam in liszt):
+#         return True
+#     else:
+#         return False
 
-szamok = [1, 7, 15, 9, 2]
+
+def paros_e(liszta):
+    for szam in liszta:
+        if szam % 2 == 0:
+            return True
+
+    return False
+
+
+szamok = [1, 7, 15, 9]
 print(paros_e(szamok))
